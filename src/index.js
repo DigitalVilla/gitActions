c6871a0465d5772e6ac3283346036070d4c9c624
DigitalVilla <digitalvilla01@gmail.com>
2020-11-26 19:22:52 -0700

    Update test action dist

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 const { replaceAsync, execAsync } = require('./utils')
 const core = require('@actions/core')
-const github = require('@actions/github')
+// const github = require('@actions/github')
 
 async function main() {
   try {
-    const scriptToRun = core.getInput('script')
+    const appName = core.getInput('app')
 
-    const sls = await execAsync(`npm i -g serverless`)
+    let script = await execAsync(`yarn add scripty -D && npm i -g serverless`)
+    console.log(sls.stdout)
+    script = await execAsync(
+      `serverless config credentials --provider aws --key ${process.env.AWS_ACCESS_KEY_ID} --secret ${process.env.AWS_SECRET_ACCESS_KEY}`
+    )
     console.log(sls.stdout)
 
     const env = process.env.GITHUB_REF.indexOf('master') > 0 ? 'prod' : 'dev'
     process.env.NODE_ENV = env
     console.log(process.env)
 
-    let script = ''
-
-    if (scriptToRun.length) {
-      console.log(`Deploying ${scriptToRun.split(' '[0])}`)
-      script = await execAsync(`yarn ${scriptToRun} --stage ${env}`)
+    if (appName.length) {
+      console.log(`Deploying app=${appName} env=${env}`)
+      script = await execAsync(`yarn sls:test ${appName} --stage ${env}`)
     }
 
     core.setOutput('update', script.stdout)
     core.setOutput('env', process.env)
     // Get the JSON webhook payload for the event that triggered the workflow
-    const context = JSON.stringify(github.ref, undefined, 2)
-    console.log(`The event context: ${context}`)
+    // const context = JSON.stringify(github.ref, undefined, 2)
+    // console.log(`The event context: ${context}`)
   } catch (error) {
     core.setFailed(error.message)
   }
