4929a780e0743bb003af74c049d8b1c5d70ed5d2
DigitalVilla <digitalvilla01@gmail.com>
2020-11-26 18:59:31 -0700

    Update test action dist

--- src/index.js
+++ src/index.js
@@ -6,19 +6,24 @@ async function main() {
   try {
     const scriptToRun = core.getInput('script')
 
-    console.log(process.env)
-    const env = process.env.GITHUB_BASE_REF === 'master' ? 'prod' : 'dev'
-    console.log(env)
+    const sls = await execAsync(`npm i -g serverless`)
+    console.log(sls.stdout)
+
+    const env = process.env.GITHUB_REF.indexOf('master') > 0 ? 'prod' : 'dev'
     process.env.NODE_ENV = env
+    console.log(process.env)
 
     let script = ''
-    if (scriptToRun) {
+
+    if (scriptToRun.length) {
+      console.log(`Deploying ${scriptToRun.split(' '[0])}`)
       script = await execAsync(`yarn ${scriptToRun} --stage ${env}`)
     }
 
     core.setOutput('update', script.stdout)
+    core.setOutput('env', process.env)
     // Get the JSON webhook payload for the event that triggered the workflow
-    const context = JSON.stringify(github, undefined, 2)
+    const context = JSON.stringify(github.ref, undefined, 2)
     console.log(`The event context: ${context}`)
   } catch (error) {
     core.setFailed(error.message)
